38335d3bcd92c78f5de44a9015749d4f82ff055f
mareksuscak <marek.suscak@gmail.com>
2017-10-20 15:51:00 +0200

    Move webpack.config.js that relates to demo

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,7 +2,7 @@ var path          = require('path')
 var webpack       = require('webpack')
 
 module.exports = {
-  entry: './demo/entry.js',
+  entry: path.join(__dirname, './entry.js'),
 
   output: {
     path: path.join(__dirname, 'demo'),
